f57c9db2cac3113ed59627014723bb3d6ee19404
eomttt <hyunt0413@gmail.com>
2021-06-17 23:44:41 +0900

    File path test

--- pages/api/blog.tsx
+++ pages/api/blog.tsx
@@ -1,8 +1,11 @@
-const fs = require("fs");
+const fs = require('fs');
 
 export default function handler(req: any, res: any) {
-  const item = fs.readFileSync(`${process.cwd()}/components/Layout.tsx`, "utf8");
-  const list = fs.readFileSync(`${process.cwd()}/public/vercel.svg`, "utf8");
+  const item = fs.readFileSync(
+    `${process.cwd()}/components/Layout.tsx`,
+    'utf8',
+  );
+  const list = fs.readFileSync(`${process.cwd()}/public/vercel.svg`, 'utf8');
 
-  res.status(200).json({ name: list, item });
+  res.status(200).json({ name: process.cwd(), item: __dirname });
 }
