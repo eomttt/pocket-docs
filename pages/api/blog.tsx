b614a65273300deb99295da86be614f26600f493
eomttt <hyunt0413@gmail.com>
2021-06-17 23:26:11 +0900

    Get file from components

--- pages/api/blog.tsx
+++ pages/api/blog.tsx
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
 export default function handler(req: any, res: any) {
+  const item = fs.readFileSync(`${process.cwd()}/components/Layout.tsx`, "utf8");
   const list = fs.readFileSync(`${process.cwd()}/public/vercel.svg`, "utf8");
-  console.log("CHECK", list);
 
-  res.status(200).json({ name: list });
+  res.status(200).json({ name: list, item });
 }
